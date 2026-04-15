990fd9f6e80653c3b5d6d7914ef03383394c147f
cuongdata <nguyenchicuongpy2021@gmail.com>
2026-04-15 22:46:49 +0700

    setup bottom navigation

--- app/src/main/java/com/nhom/travelapp/MainActivity.kt
+++ app/src/main/java/com/nhom/travelapp/MainActivity.kt
@@ -1,43 +1,39 @@
 package com.nhom.travelapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.nhom.travelapp.core.firebase.FirebaseProvider
-import com.nhom.travelapp.databinding.ActivityMainBinding
-import com.nhom.travelapp.ui.auth.login.LoginActivity
-import com.nhom.travelapp.data.repository.AuthRepository
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.nhom.travelapp.ui.discovery.DiscoveryFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private val authRepository = AuthRepository()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        setupViews()
-    }
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
-    private fun setupViews() {
-        val currentUser = FirebaseProvider.auth.currentUser
-        val email = currentUser?.email ?: "Người dùng"
+        // Mở app lên thì mặc định vào thẳng tab Khám Phá
+        if (savedInstanceState == null) {
+            replaceFragment(DiscoveryFragment())
 
-        binding.tvWelcome.text = "Xin chào, $email"
+            bottomNav.selectedItemId = R.id.nav_discovery
+        }
 
-        binding.btnLogout.setOnClickListener {
-            logout()
+        // Sự kiện bấm đổi Tab
+        // Mỗi tab nên sử dụng fragment
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_discovery -> replaceFragment(DiscoveryFragment())
+            }
+            true
         }
     }
 
-    private fun logout() {
-        authRepository.logout()
-
-        val intent = Intent(this, LoginActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        startActivity(intent)
-        finish()
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
