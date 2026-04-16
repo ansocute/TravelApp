e4df0d4f7978e875a65656209f6e881cba2a481d
NHTung-0801 <kiritohackiem05@gmail.com>
2026-04-16 08:26:57 +0700

    dong bo nhanh main va xu ly bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,11 @@ dependencies {
     // Thư viện để gọi popup đăng nhập Google
     implementation("com.google.android.gms:play-services-auth:21.0.0")
     // ---------------------------------
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+    implementation("com.google.firebase:firebase-firestore")
+
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
